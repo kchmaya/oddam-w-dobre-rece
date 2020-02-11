9f5d8a527d31289aa5e00f196c3d9d60de12df64
kchmaya <kahnovichmaya@gmail.com>
2020-02-11 17:26:02 +0100

    header changes

---
+++ src/components/Header.js
@@ -0,0 +1,57 @@
+import React, {Component} from 'react';
+import {NavLink} from 'react-router-dom';
+import {Link} from 'react-scroll';
+
+class Header extends Component {
+    render() {
+        return (
+            <div className='header'>
+                <div className='header-login'>
+                    <ul>
+                        <li>
+                            <NavLink className="header-login-nav" to="/login">
+                                Zaloguj
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink className="header-login-nav" to="/register">
+                                Załóż konto
+                            </NavLink>
+                        </li>
+                    </ul>
+                </div>
+                <div className='header-menu'>
+                    <ul>
+                        <li>
+                            <Link to="/">
+                                Start
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="">
+                                O co chodzi?
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="">
+                                O nas
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="">
+                                Fundacja i organizacje
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="">
+                                Kontakt
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Header;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,28 +1,27 @@
 import React, {Component} from 'react';
 import Header from './Header';
-import MainSection from './MainSection';
-import ThreeColumns from './ThreeColumns';
-import Steps from './Steps';
-import About from './About';
-import Organisations from './Organisations';
-import Contact from './Contact';
-import Footer from './Footer';
+// import MainSection from './MainSection';
+// import ThreeColumns from './ThreeColumns';
+// import Steps from './Steps';
+// import About from './About';
+// import Organisations from './Organisations';
+// import Contact from './Contact';
+// import Footer from './Footer';
 
 
 class Home extends Component {
     render() {
         return (
-            <div>
-                <p>HomePage</p>   */
+            <>
                 <Header />
-                <MainSection />
-                <ThreeColumns />
-                <Steps />
-                <About />
-                <Organisations />
-                <Contact />
-                <Footer />
-            </div>
+            {/*    <MainSection />*/}
+            {/*    <ThreeColumns />*/}
+            {/*    <Steps />*/}
+            {/*    <About />*/}
+            {/*    <Organisations />*/}
+            {/*    <Contact />*/}
+            {/*    <Footer />*/}
+            </>
         );
     }
 }
