e0e874de5d509e02ef8737cd9cfe5f7c39cfcb17
kchmaya <kahnovichmaya@gmail.com>
2020-02-12 13:15:11 +0100

    three column section - done

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import Header from './Header';
 import MainSection from './MainSection';
-// import ThreeColumns from './ThreeColumns';
+import ThreeColumns from './ThreeColumns';
 // import Steps from './Steps';
 // import About from './About';
 // import Organisations from './Organisations';
@@ -15,7 +15,7 @@ class Home extends Component {
             <>
                 <Header />
                 <MainSection />
-            {/*    <ThreeColumns />*/}
+                <ThreeColumns />
             {/*    <Steps />*/}
             {/*    <About />*/}
             {/*    <Organisations />*/}
--- src/components/MainSection.js
+++ src/components/MainSection.js
@@ -7,13 +7,16 @@ class MainSection extends Component {
             <section className='main-section'>
                 <img src={require ('../assets/Home-Hero-Image.jpg')} alt='' className='main-section-img'/>
                 <div className='main-section-text'>
-                    <h1 className='main-section-title'> Zacznij pomagać! Oddaj niechciane rzeczy w zaufane ręce </h1>
+                    <h1 className='main-section-title'> Zacznij pomagać!
+                        <span>Oddaj niechciane rzeczy w zaufane ręce </span>
+                    </h1>
+                    <img src={require ('../assets/Decoration.svg')} alt='' className='decoration'/>
                     <ul className='main-section-btns'>
                         <li>
-                            <Link to='' className='btn-link'> Oddaj rzeczy </Link>
+                            <Link to='/login' className='btn-link'> Oddaj <span>rzeczy</span> </Link>
                         </li>
                         <li>
-                            <Link to='' className='btn-link'> Zorganizuj zbiórkę </Link>
+                            <Link to='/login' className='btn-link'> Zorganizuj <span>zbiórkę</span> </Link>
                         </li>
                     </ul>
                 </div>
