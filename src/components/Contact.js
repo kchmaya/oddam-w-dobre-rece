99d669ce8992d0f685f616e671289ed4d7c0022b
kchmaya <kahnovichmaya@gmail.com>
2020-02-16 16:30:49 +0100

    form validation

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,26 +1,123 @@
 import React, {Component} from 'react';
 
 class Contact extends Component {
+
+    state = {
+        name: "",
+        email: "",
+        message: "",
+        errorName: "",
+        errorEmail: "",
+        errorText: "",
+        successMessage: ""
+    };
+
+    validateEmail = email => {
+        let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(String(email).toLowerCase());
+    };
+
+    handleChange = event => {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    };
+
+    handleSubmit = event => {
+        event.preventDefault();
+
+        this.setState({
+            errorName: "",
+            errorEmail: "",
+            errorText: "",
+            successMessage: ""
+        });
+
+        const data = {
+            name: this.state.name,
+            email: this.state.email,
+            message: this.state.message
+        };
+
+        console.log(data);
+
+        let nameOk = true;
+        let emailOk = true;
+        let textOk = true;
+
+        if (data.name.indexOf(" ") !== 0) {
+            this.setState(state => ({
+                errorName: "Podane imię jest nieprawidłowe!"
+            }));
+            nameOk = false;
+        }
+        if (!this.validateEmail(data.email)) {
+            this.setState(state => ({
+                errorEmail: "Podany email jest nieprawidłowy"
+            }));
+            emailOk = false;
+        }
+        if (data.message.length < 120) {
+            this.setState(state => ({
+                errorText: "Pole wiadomość musi mieć conajmniej 120 znaków!"
+            }));
+            textOk = false;
+        }
+        if (nameOk && emailOk && textOk) {
+            this.setState({
+                successMessage: "Wiadomość została wysłana! Wkrótce się skontaktujemy."
+            });
+
+            const url = "https://fer-api.coderslab.pl/v1/portfolio/contact";
+
+            return fetch(url, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(data)
+            })
+                .then(response => {
+                    if (response.ok) {
+                        return response.json();
+                    } else {
+                        throw new Error("Blad sieci");
+                    }
+                })
+                .catch(error => {
+                    console.log(error);
+                });
+        }
+    };
+
     render() {
         return (
             <section className='contact-section' id='contact'>
                 <div className='contact-form'>
                     <h2 className='contact-title'> Skontaktuj się z nami </h2>
                     <img src={require('../assets/Decoration.svg')} alt='' className='contact-decoration'/>
-                    <form className='form'>
+                    <p style={{color: "green"}}> {this.state.successMessage} </p>
+                    <form className='form' onSubmit={this.handleSubmit}>
                         <div className='form-personal'>
-                                <label> Wpisz swoje imię
-                                    <input placeholder='name'/>
-                                </label>
-                                <label> Wpisz swój email
-                                    <input placeholder='email'/>
-                                </label>
+                            <label> Wpisz swoje imię
+                                <input type='text' name='name' value={this.state.name}
+                                       placeholder='name' onChange={this.handleChange}/>
+                                <p style={{color: "red"}}> {this.state.errorName} </p>
+                            </label>
+                            <label> Wpisz swój email
+                                <input type='email' name='email' value={this.state.email}
+                                       placeholder='email' onChange={this.handleChange}/>
+                                <p style={{color: "red"}}> {this.state.errorEmail} </p>
+                            </label>
                         </div>
                         <label className='form-textarea'> Wpisz swoją wiadomość
-                            <textarea placeholder='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                        ullamco laboris nisi ut aliquip ex ea commodo consequat.' maxLength='200'>
+                            <textarea name='message' value={this.state.message}
+                                      onChange={this.handleChange}
+                                      placeholder='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                                                   incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+                                                   nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'>
                              </textarea>
+                            <p style={{color: "red"}}> {this.state.errorText} </p>
                         </label>
                         <button className='contact-form-btn' type="submit" value="Wyślij"> Wyślij</button>
                     </form>
--- src/components/Organisations.js
+++ src/components/Organisations.js
@@ -1,154 +1,190 @@
 import React, {Component} from 'react';
-import {Link} from "react-router-dom";
+import {NavLink, Switch, Route} from 'react-router-dom';
+import Login from "./UserLog/Login";
+
+
+const Fundations = () => {
+
+    const fundationsInfo = [
+        {
+            name: `Fundacja "Lorem Ipsum 1"`,
+            description: "  Donec nec massa eu arcu dapibus placerat aliquet id lacus. Vestibulum nec pretium ante.",
+            additional: "Vestibulum vel leo lacus."
+        },
+        {
+            name: `Fundacja "Lorem Ipsum 2"`,
+            description: "  Donec nec massa eu arcu dapibus placerat aliquet id lacus. Vestibulum nec pretium ante.",
+            additional: "Vestibulum vel leo lacus."
+        },
+        {
+            name: `Fundacja "Lorem Ipsum 3"`,
+            description: "  Donec nec massa eu arcu dapibus placerat aliquet id lacus. Vestibulum nec pretium ante.",
+            additional: "Vestibulum vel leo lacus."
+        },
+        {
+            name: `Fundacja "Lorem Ipsum 4"`,
+            description: "  Donec nec massa eu arcu dapibus placerat aliquet id lacus. Vestibulum nec pretium ante.",
+            additional: "Vestibulum vel leo lacus."
+        },
+        {
+            name: `Fundacja "Lorem Ipsum 5"`,
+            description: "  Donec nec massa eu arcu dapibus placerat aliquet id lacus. Vestibulum nec pretium ante.",
+            additional: "Vestibulum vel leo lacus."
+        },
+        {
+            name: `Fundacja "Lorem Ipsum 6"`,
+            description: "  Donec nec massa eu arcu dapibus placerat aliquet id lacus. Vestibulum nec pretium ante.",
+            additional: "Vestibulum vel leo lacus."
+        },
+        {
+            name: `Fundacja "Lorem Ipsum 7"`,
+            description: "  Donec nec massa eu arcu dapibus placerat aliquet id lacus. Vestibulum nec pretium ante.",
+            additional: "Vestibulum vel leo lacus."
+        },
+        {
+            name: `Fundacja "Lorem Ipsum 8"`,
+            description: "  Donec nec massa eu arcu dapibus placerat aliquet id lacus. Vestibulum nec pretium ante.",
+            additional: "Vestibulum vel leo lacus."
+        },
+        {
+            name: `Fundacja "Lorem Ipsum 9"`,
+            description: "  Donec nec massa eu arcu dapibus placerat aliquet id lacus. Vestibulum nec pretium ante.",
+            additional: "Vestibulum vel leo lacus."
+        },
+    ];
+
+    return (
+        fundationsInfo.map((data, index) => {
+            return <div key={index}>
+                <h4>{data.name}</h4>
+                <p>{data.description}</p>
+                <p>{data.additional}</p>
+            </div>
+        })
+    )
+};
+
+const NonGovOrganisations = () => {
+
+    const organisationsInfo = [
+        {
+            name: `Organizacja "Lorem Ipsum 1"`,
+            description: "Sed molestie sit amet nisi ac venenatis. Suspendisse potenti.",
+            additional: "Nam laoreet consectetur nunc"
+        },
+        {
+            name: `Organizacja "Lorem Ipsum 2"`,
+            description: " Vestibulum nec pretium ante. Donec nec massa eu arcu dapibus placerat aliquet id lacus.",
+            additional: "Vestibulum vel leo lacus."
+        },
+        {
+            name: `Organizacja "Lorem Ipsum 3"`,
+            description: "  Donec nec massa eu arcu dapibus placerat aliquet id lacus. Vestibulum nec pretium ante.",
+            additional: "Vestibulum vel leo lacus."
+        },
+        {
+            name: `Organizacja "Lorem Ipsum 4"`,
+            description: "  Donec nec massa eu arcu dapibus placerat aliquet id lacus. Vestibulum nec pretium ante.",
+            additional: "Vestibulum vel leo lacus."
+        },
+        {
+            name: `Organizacja "Lorem Ipsum 5"`,
+            description: "  Donec nec massa eu arcu dapibus placerat aliquet id lacus. Vestibulum nec pretium ante.",
+            additional: "Vestibulum vel leo lacus."
+        },
+        {
+            name: `Organizacja "Lorem Ipsum 6"`,
+            description: "  Donec nec massa eu arcu dapibus placerat aliquet id lacus. Vestibulum nec pretium ante.",
+            additional: "Vestibulum vel leo lacus."
+        }
+    ];
+
+    return (
+        organisationsInfo.map((data, index) => {
+            return <div key={index}>
+                <h4>{data.name}</h4>
+                <p>{data.description}</p>
+                <p>{data.additional}</p>
+            </div>
+        })
+    )
+};
+
+const LocalOrganisations = () => {
+
+    const localOrganisationsInfo = [
+        {
+            name: `Zbiórka "Lorem Ipsum 1"`,
+            description: "  Donec nec massa eu arcu dapibus placerat aliquet id lacus. Vestibulum nec pretium ante.",
+            additional: "Vestibulum vel leo lacus."
+        },
+        {
+            name: `Zbiórka "Lorem Ipsum 2"`,
+            description: "  Donec nec massa eu arcu dapibus placerat aliquet id lacus. Vestibulum nec pretium ante.",
+            additional: "Vestibulum vel leo lacus."
+        },
+        {
+            name: `Zbiórka "Lorem Ipsum 3"`,
+            description: "  Donec nec massa eu arcu dapibus placerat aliquet id lacus. Vestibulum nec pretium ante.",
+            additional: "Vestibulum vel leo lacus."
+        },
+
+    ];
+    return (
+        localOrganisationsInfo.map((data, index) => {
+            return <div key={index}>
+                <h4>{data.name}</h4>
+                <p>{data.description}</p>
+                <p>{data.additional}</p>
+            </div>
+        })
+    )
+};
 
 
 class Organisations extends Component {
-    state = {
-        data: {
-            'organisations': [
-                {
-                    name: `Organizacja "Lorem Ipsum 1"`,
-                    description: "Sed molestie sit amet nisi ac venenatis. Suspendisse potenti.",
-                    additional: "Nam laoreet consectetur nunc"
-                },
-                {
-                    name: `Organizacja "Lorem Ipsum 2"`,
-                    description: " Vestibulum nec pretium ante. Donec nec massa eu arcu dapibus placerat aliquet id lacus.",
-                    additional: "Vestibulum vel leo lacus."
-                },
-                {
-                    name: `Organizacja "Lorem Ipsum 3"`,
-                    description: "  Donec nec massa eu arcu dapibus placerat aliquet id lacus. Vestibulum nec pretium ante.",
-                    additional: "Vestibulum vel leo lacus."
-                },
-                {
-                    name: `Organizacja "Lorem Ipsum 4"`,
-                    description: "  Donec nec massa eu arcu dapibus placerat aliquet id lacus. Vestibulum nec pretium ante.",
-                    additional: "Vestibulum vel leo lacus."
-                },
-                {
-                    name: `Organizacja "Lorem Ipsum 5"`,
-                    description: "  Donec nec massa eu arcu dapibus placerat aliquet id lacus. Vestibulum nec pretium ante.",
-                    additional: "Vestibulum vel leo lacus."
-                },
-                {
-                    name: `Organizacja "Lorem Ipsum 6"`,
-                    description: "  Donec nec massa eu arcu dapibus placerat aliquet id lacus. Vestibulum nec pretium ante.",
-                    additional: "Vestibulum vel leo lacus."
-                }
-            ],
 
-            'foundations': [
-                {
-                    name: `Fundacja "Lorem Ipsum 1"`,
-                    description: "  Donec nec massa eu arcu dapibus placerat aliquet id lacus. Vestibulum nec pretium ante.",
-                    additional: "Vestibulum vel leo lacus."
-                },
-                {
-                    name: `Fundacja "Lorem Ipsum 2"`,
-                    description: "  Donec nec massa eu arcu dapibus placerat aliquet id lacus. Vestibulum nec pretium ante.",
-                    additional: "Vestibulum vel leo lacus."
-                },
-                {
-                    name: `Fundacja "Lorem Ipsum 3"`,
-                    description: "  Donec nec massa eu arcu dapibus placerat aliquet id lacus. Vestibulum nec pretium ante.",
-                    additional: "Vestibulum vel leo lacus."
-                },
-                {
-                    name: `Fundacja "Lorem Ipsum 4"`,
-                    description: "  Donec nec massa eu arcu dapibus placerat aliquet id lacus. Vestibulum nec pretium ante.",
-                    additional: "Vestibulum vel leo lacus."
-                },
-                {
-                    name: `Fundacja "Lorem Ipsum 5"`,
-                    description: "  Donec nec massa eu arcu dapibus placerat aliquet id lacus. Vestibulum nec pretium ante.",
-                    additional: "Vestibulum vel leo lacus."
-                },
-                {
-                    name: `Fundacja "Lorem Ipsum 6"`,
-                    description: "  Donec nec massa eu arcu dapibus placerat aliquet id lacus. Vestibulum nec pretium ante.",
-                    additional: "Vestibulum vel leo lacus."
-                },
-                {
-                    name: `Fundacja "Lorem Ipsum 7"`,
-                    description: "  Donec nec massa eu arcu dapibus placerat aliquet id lacus. Vestibulum nec pretium ante.",
-                    additional: "Vestibulum vel leo lacus."
-                },
-                {
-                    name: `Fundacja "Lorem Ipsum 8"`,
-                    description: "  Donec nec massa eu arcu dapibus placerat aliquet id lacus. Vestibulum nec pretium ante.",
-                    additional: "Vestibulum vel leo lacus."
-                },
-                {
-                    name: `Fundacja "Lorem Ipsum 9"`,
-                    description: "  Donec nec massa eu arcu dapibus placerat aliquet id lacus. Vestibulum nec pretium ante.",
-                    additional: "Vestibulum vel leo lacus."
-                },
-            ],
+    showOnClick(content) {
+        // content.preventDefault();
+        this.setState({currentContent: content})
+    }
 
-            'localCollections': [
-                {
-                    name: `Zbiórka "Lorem Ipsum 1"`,
-                    description: "  Donec nec massa eu arcu dapibus placerat aliquet id lacus. Vestibulum nec pretium ante.",
-                    additional: "Vestibulum vel leo lacus."
-                },
-                {
-                    name: `Zbiórka "Lorem Ipsum 2"`,
-                    description: "  Donec nec massa eu arcu dapibus placerat aliquet id lacus. Vestibulum nec pretium ante.",
-                    additional: "Vestibulum vel leo lacus."
-                },
-                {
-                    name: `Zbiórka "Lorem Ipsum 3"`,
-                    description: "  Donec nec massa eu arcu dapibus placerat aliquet id lacus. Vestibulum nec pretium ante.",
-                    additional: "Vestibulum vel leo lacus."
-                },
-
-            ],
-        },
-
-        currentType: "organisations",
-        organisationPage: 0,
-    };
-
-    changePage = (num) => {
-        this.setState({organisationsPage: num})
-    };
-
-    createPageButtons = (num) => {
-        if (num < 2) {
-            return null;
-        } else {
-            let buttons = [];
-            for (let i = 0; i < num; i++) {
-                buttons.push(<button key={i} className={(this.state.organisationsPage === i) ? "active" : "passive"}
-                                     onClick={() => {this.changePage(i)}}>{i + 1}</button>)
-            }
-            return buttons;
-        }
-    };
 
     render() {
-        const data=this.state.data;
-        const type=this.state.currentType;
-        const pageNum = Math.ceil(data[type].length / 3);
+        // const content = this.state.currentContent;
+
         return (
             <section className='organisation-section' id='organisation'>
-                <h2 className='organisation-title'> Komu pomagamy? </h2>
-                <img src={require('../assets/Decoration.svg')} alt='' className='organisation-decoration'/>
-                <ul className='organisation-choice'>
-                    <li>
-                        <Link to='/' className=''> Fundacjom </Link>
-                    </li>
-                    <li>
-                        <Link to='/' className=''> Organizacjom <span> pozarządowym </span> </Link>
-                    </li>
-                    <li>
-                        <Link to='/' className=''> Lokalnym <span> zbiórkom </span> </Link>
-                    </li>
-                </ul>
-                <p></p>
-                <div className='page-num'>{this.createPageButtons(pageNum)}</div>
+                <div className='organisation-header'>
+                    <h2 className='organisation-title'> Komu pomagamy? </h2>
+                    <img src={require('../assets/Decoration.svg')} alt='' className='organisation-decoration'/>
+                    <ul className='organisation-choice'>
+                        <li>
+                            <NavLink to='' onClick={()=> this.showOnClick('fundations')} className='organisation-link'> Fundacjom</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to='' onClick={this.showOnClick} className='organisation-link'> Organizacjom <span> pozarządowym </span></NavLink>
+                        </li>
+                        <li>
+                            <NavLink to='' onClick={this.showOnClick} className='organisation-link'> Lokalnym <span> zbiórkom </span></NavLink>
+                        </li>
+                    </ul>
+                    <p className='organisation-description'> Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                        sed do eiusmod tempor incididunt ut
+                        laboreet dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation. </p>
+                </div>
+                <div className='organisations'>
+                    {/*<Switch>*/}
+                    {/*    <Route  path='/' component={() => <Fundations />} />*/}
+                    {/*    <Route  path='/' component={() => <NonGovOrganisations />} />*/}
+                    {/*    <Route  path='/' component={() => <LocalOrganisations/>} />*/}
+                    {/*</Switch>*/}
 
+                    {/*<Fundations />*/}
+
+                    {/*<NonGovOrganisations/>*/}
+                    {/*<LocalOrganisations/>*/}
+                </div>
             </section>
         )
     }
