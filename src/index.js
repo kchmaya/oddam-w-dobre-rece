7806038928cb792f49945baad40a1663285d0a45
kchmaya <kahnovichmaya@gmail.com>
2020-04-06 16:26:24 +0200

    firebase authentication added

--- src/index.js
+++ src/index.js
@@ -3,7 +3,12 @@ import ReactDOM from 'react-dom';
 import "./scss/main.scss";
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Firebase, { FirebaseContext } from "./firebase";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <FirebaseContext.Provider value={new Firebase()}>
+        <App/>
+    </FirebaseContext.Provider>,
+    document.getElementById('root'));
 
 serviceWorker.unregister();
