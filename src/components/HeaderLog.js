ca31106270c6573162bdf0878fad1418aa5d753d
kchmaya <kahnovichmaya@gmail.com>
2020-04-16 18:20:02 +0200

    logout logic added

--- src/components/HeaderLog.js
+++ src/components/HeaderLog.js
@@ -1,7 +1,21 @@
 import React, {Component} from 'react';
 import {NavLink} from 'react-router-dom';
+import Firebase from "../firebase/firebase";
 
 class HeaderLog extends Component {
+
+    logOut() {
+        Firebase.auth().signOut().then((user) => {
+            if (user) {
+                this.setState({
+                    user: null,
+                }).catch((error) => {
+                    console.log(error)
+                });
+            }
+        })
+    }
+
     render() {
         return (
             <section className='header'>
@@ -9,12 +23,12 @@ class HeaderLog extends Component {
                     <ul>
                         <li> Cześć, {this.props.email} </li>
                         <li>
-                            <NavLink className='header-login-nav' to='/login'>
+                            <NavLink className='header-login-nav' to='/'>
                                 Oddaj rzeczy
                             </NavLink>
                         </li>
                         <li>
-                            <NavLink className='header-login-nav' to='/logout'>
+                            <NavLink className='header-login-nav' to='/logout' onClick={this.logOut}>
                                 Wyloguj
                             </NavLink>
                         </li>
